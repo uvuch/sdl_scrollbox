7474a3ed90b195609c5f9ef995b2a5d26efdf4aa
Serge Muzyka <siarhei.muzyka@pennmedicine.upenn.edu>
2023-08-17 19:55:23 -0400

    Initial Commit

---
+++ SDL_scrollbox/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SDL_scrollbox
+//
+//  Created by Serge Muzyka on 8/17/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
